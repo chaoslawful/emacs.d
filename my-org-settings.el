169d63dec1befd84f7ce6fb751233a4c14d016c7
Wang Xiaozhe <chaoslawful@gmail.com>
2013-03-29 16:36:46 +0800

    更新个人配置

--- my-org-settings.el
+++ my-org-settings.el
@@ -10,6 +10,58 @@
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
 
+;; 设置中英文等宽字体
+(defun qiang-font-existsp (font)
+  (if (null (x-list-fonts font))
+	nil t))
+
+(defun qiang-make-font-string (font-name font-size)
+  (if (and (stringp font-size) 
+		   (equal ":" (string (elt font-size 0))))
+	(format "%s%s" font-name font-size)
+	(format "%s-%s" font-name font-size)))
+
+(defun qiang-set-font (english-fonts
+						english-font-size
+						chinese-fonts)
+  "english-font-size could be set to \":pixelsize=18\" or a integer.
+If set/leave chinese-font-size to nil, it will follow english-font-size"
+(require 'cl)                         ; for find if
+(let ((en-font (qiang-make-font-string
+				 (find-if #'qiang-font-existsp english-fonts)
+				 english-font-size))
+	  (zh-font (font-spec :family (find-if #'qiang-font-existsp chinese-fonts))))
+
+  ;; Set the default English font
+  ;; 
+  ;; The following 2 method cannot make the font settig work in new frames.
+  ;; (set-default-font "Consolas:pixelsize=18")
+  ;; (add-to-list 'default-frame-alist '(font . "Consolas:pixelsize=18"))
+  ;; We have to use set-face-attribute
+  (message "Set English Font to %s" en-font)
+  (set-face-attribute
+	'default nil :font en-font)
+
+  ;; Set Chinese font 
+  ;; Do not use 'unicode charset, it will cause the english font setting invalid
+  (message "Set Chinese Font to %s" zh-font)
+  (dolist (charset '(kana han symbol cjk-misc bopomofo))
+	(set-fontset-font t
+					  charset
+					  zh-font))))
+
+(setq face-font-rescale-alist '(("Microsoft Yahei" . 1.2) ("WenQuanYi Zen Hei" . 1.2)))
+(qiang-set-font
+  '("Monaco" "Consolas" "DejaVu Sans Mono" "Monospace" "Courier New") 10.5
+  '("文泉驿等宽微米黑" "Microsoft Yahei" "黑体" "新宋体" "宋体"))
+
+
+(set-face-attribute 'default nil :font (font-spec))
+
+; here are 20 hanzi and 40 english chars, see if they are the same width
+; 你你你你你你你你你你你你你你你你你你你你
+; aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
 ;; 默认以缩进方式打开 org 文件
 (setq org-startup-indented t)
 
@@ -96,7 +148,10 @@
                                (clojure . t)
                                (ledger . t)
                                (org . t)
-                               (plantuml . t)))
+                               (plantuml . t)
+                               (asymptote . t)
+                               (octave . t)
+                               (lilypond . t)))
 
 ;; LaTeX export
 (unless (boundp 'org-export-latex-classes)
