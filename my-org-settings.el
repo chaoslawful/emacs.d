08f107b5efc803e8691736656f2e89581204fa7f
Wang Xiaozhe <chaoslawful@gmail.com>
2013-03-29 21:06:49 +0800

    重新配置中英文等宽字体

--- my-org-settings.el
+++ my-org-settings.el
@@ -11,56 +11,19 @@
 (global-set-key "\C-cb" 'org-iswitchb)
 
 ;; 设置中英文等宽字体
-(defun qiang-font-existsp (font)
-  (if (null (x-list-fonts font))
-	nil t))
+;; 设置默认英文字体为 Consolas，大小 11pt
+(set-default-font "Consolas 11")
+;; 前面一串“(if...lambda...(with-select-frame frame ())...)"是个很好的函数框架，
+;; 意思是frame创建后载入，用这个框架可以解决--daemon启动的问题
+;; 只有set-fontset-font一句指定修改字符集'unicode的字体为文泉驿等宽微米黑，大小 12pt
+(if (and (fboundp 'daemonp) (daemonp))
+  (add-hook 'after-make-frame-functions
+			(lambda (frame)
+			  (with-selected-frame frame
+								   (set-fontset-font "fontset-default" 
+													 'unicode "WenQuanyi Micro Hei Mono 12"))))
+  (set-fontset-font "fontset-default" 'unicode "WenQuanYi Micro Hei Mono 12"))
 
-(defun qiang-make-font-string (font-name font-size)
-  (if (and (stringp font-size) 
-		   (equal ":" (string (elt font-size 0))))
-	(format "%s%s" font-name font-size)
-	(format "%s-%s" font-name font-size)))
-
-(defun qiang-set-font (english-fonts
-						english-font-size
-						chinese-fonts)
-  "english-font-size could be set to \":pixelsize=18\" or a integer.
-If set/leave chinese-font-size to nil, it will follow english-font-size"
-(require 'cl)                         ; for find if
-(let ((en-font (qiang-make-font-string
-				 (find-if #'qiang-font-existsp english-fonts)
-				 english-font-size))
-	  (zh-font (font-spec :family (find-if #'qiang-font-existsp chinese-fonts))))
-
-  ;; Set the default English font
-  ;; 
-  ;; The following 2 method cannot make the font settig work in new frames.
-  ;; (set-default-font "Consolas:pixelsize=18")
-  ;; (add-to-list 'default-frame-alist '(font . "Consolas:pixelsize=18"))
-  ;; We have to use set-face-attribute
-  (message "Set English Font to %s" en-font)
-  (set-face-attribute
-	'default nil :font en-font)
-
-  ;; Set Chinese font 
-  ;; Do not use 'unicode charset, it will cause the english font setting invalid
-  (message "Set Chinese Font to %s" zh-font)
-  (dolist (charset '(kana han symbol cjk-misc bopomofo))
-	(set-fontset-font t
-					  charset
-					  zh-font))))
-
-(setq face-font-rescale-alist '(("Microsoft Yahei" . 1.2) ("WenQuanYi Zen Hei" . 1.2)))
-(qiang-set-font
-  '("Monaco" "Consolas" "DejaVu Sans Mono" "Monospace" "Courier New") 10.5
-  '("文泉驿等宽微米黑" "Microsoft Yahei" "黑体" "新宋体" "宋体"))
-
-
-(set-face-attribute 'default nil :font (font-spec))
-
-; here are 20 hanzi and 40 english chars, see if they are the same width
-; 你你你你你你你你你你你你你你你你你你你你
-; aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 
 ;; 默认以缩进方式打开 org 文件
 (setq org-startup-indented t)
